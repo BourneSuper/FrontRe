7ecc6c6227ff16ba2aa2d07057f5d50ce0966338
Bourne <bourne_w@sina.com>
2018-02-23 23:28:20 +0800

    test

--- demoIndex.php
+++ demoIndex.php
@@ -11,15 +11,18 @@
                 var cotrollerMethod = "ajaxLogin"
                 
                 var userId = $("input[name=userId]")[0].value;
+                var password = $("input[name=password]")[0].value;
                                 
                 $.ajax({
                     type: 'POST',
                     async: false,
                     url: "mainController.php",
                     data:{
-                        "cotrollerName":cotrollerName,
-                        "cotrollerMethod":cotrollerMethod,
-                        "userId":userId
+                        "cotrollerName" : cotrollerName,
+                        "cotrollerMethod" : cotrollerMethod,
+                        "userId" : userId,
+                        "password" : password,
+                        
                     } ,
                     success: function(data, staus, xhr){
 //                         console.dir(xhr)
@@ -44,6 +47,7 @@
                                 类名：<input type="text" name='cotrollerName' value="FirstComponentController" size="30"/><br/>
                                 方法名：<input type="text" name='cotrollerMethod' value="login"/><br/>
                                 用户名：<input type="text" name='userId'  /><br/>
+                                密码：<input type="password" name='password'  /><br/>
             
             <input type="submit" value="提交"/>
         </form>
