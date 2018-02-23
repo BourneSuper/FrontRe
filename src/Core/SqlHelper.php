480c71397bed9ac095224a30a70bd03ec703246b
Bourne <bourne_w@sina.com>
2018-02-24 01:08:40 +0800

    debug

--- src/Core/SqlHelper.php
+++ src/Core/SqlHelper.php
@@ -29,7 +29,7 @@ class SqlHelper{
         
         $_SESSION['sqlHelper'] = $this;
         
-        
+var_dump( mysqli_get_connection_stats($this->link));
     }
     
     public static function getSqlHelper(){
@@ -46,7 +46,7 @@ class SqlHelper{
     
     public function query($sql){
         $this->keepLink();
-var_dump($this->link);        
+var_dump( mysqli_get_connection_stats($this->link));        
         mysqli_query( $this->link, "set NAMES 'utf8'" );
         
         $res = mysqli_query( $this->link, $sql );
